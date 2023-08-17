7f6ea96ac0710efd778ba1f9a0c4c4ae5ff9f69a
joaldanaendo <johanny.aldana.endo@gmail.com>
2023-08-16 23:07:16 -0500

    Update - Readme, HTML y JS

--- README.md
+++ README.md
@@ -1,12 +1,30 @@
 # PreEntrega2-AldanaEndo
 Repositorio para la entrega de trabajos del curso de JavaScript
 
-# Inicio de proceso 
+# Proceso 
 -- Agregar a arreglo data de una lista para poder consultarla
 -- Almacenar esta información para los filtros y cálculos
+-- Inicio de la interacción con el usuario
 -- Solicitar usuario para identificar al comprador
--- Agregar al carrito de compras los productos filtrados
--- Salis y sumar los costos
+-- Agregar al carrito de compras los productos filtrado
+-- Sumar los costos
 -- Valida el monto para agregar un descuento
 -- Mostrar el total 
 -- Cierre de interacción
+
+# Validación de usuario
+-- Solo se pide que el usuario ingrese un username
+
+# Listado de productos
+-- producto: "1", nombre: "Remera", tipo/marca: "Ropa - Tranca", costo: 200,
+-- producto: "2", nombre: "Casaca", tipo/marca: "Ropa - Tranca", costo: 100,
+-- producto: "3", nombre: "Pantalón", tipo/marca: "Ropa - Tranca", costo: 150,
+-- producto: "4", nombre: "Short", tipo/marca: "Ropa - Tranca", costo: 250,
+-- producto: "5", nombre: "Gorro", tipo/marca: "Accesorios - Act4u", costo: 45,
+-- producto: "6", nombre: "Lentes de sol", tipo/marca: "Accesorios - Act4u", costo:90,
+-- producto: "7", nombre: "Mochila", tipo/marca: "Accesorios - Act4u", costo: 120,
+-- producto: "8", nombre: "Audífonos", tipo/marca: "Tecnología - Now", costo: 50,
+-- producto: "9", nombre: "Parlante", tipo/marca: "Tecnología - Now", costo: 100,
+
+# Descuento
+-- De acuerdo al monto, si el total alcanza o supera los $500, se le aplica un 25% de descuento 
--- js/main.js
+++ js/main.js
@@ -1,106 +1,116 @@
-//** Se declara una variable que va a contener un arreglo con los objetos que se traeran desde la data */
-const list = [];
+// Se declara una variable que va a contener un arreglo con los objetos que se traeran desde la data
+const productList = [];
 
-//* Creación de clase constructora para la estructura de los objetos del arreglo "list" */
-class Juegos {
-    constructor(gameId, name, type, cost) {
-        this.gameId = gameId;
-        this.name = name;
-        this.type = type;
-        this.cost = cost;
+// Creación de clase constructora para la estructura de los objetos del arreglo "productList"
+class Products {
+    constructor(productId, productName, productType, productCost) {
+        this.productId = productId;
+        this.productName = productName;
+        this.productType = productType;
+        this.productCost = productCost;
     }
 
-    //* Método que agrega a "list" */
-    addGame() {
-        list.push({
-            gameId: `${this.gameId}`,
-            name: `${this.name}`,
-            type: `${this.type}`,
-            cost: `${this.cost}`,
+    // Método que agrega a "productList" /
+    addProduct() {
+        productList.push({
+            productId: `${this.productId}`,
+            productName: `${this.productName}`,
+            productType: `${this.productType}`,
+            productCost: `${this.productCost}`,
         })
     }
 }
 
-//* Arreglo que contendrá la información de la data */
+// Arreglo que contendrá la información de la data
 const data = [
-    { gameId: "G291", name: "5 Hand American Blackjack", type: "blackjack", cost: 1 },
-    { gameId: "G320", name: "The Great Wall", type: "keno", cost: 1 },
-    { gameId: "G284", name: "Bonus Poker", type: "poker", cost: 1 },
-    { gameId: "G327", name: "Aces and Faces", type: "poker", cost: 1 },
-    { gameId: "G128", name: "Arctic Wolf", type: "slot", cost: 50 },
-    { gameId: "G135", name: "Circus Carnival", type: "slot", cost: 40 },
-    { gameId: "G136", name: "Barbarian Riches", type: "slot", cost: 75 },
-    { gameId: "G171", name: "Princess Of The Deep", type: "slot", cost: 88 },
-    { gameId: "G176", name: "Living The Dream Classic", type: "slot", cost: 1 },
+    { productId: "1", productName: "Remera", productType: "Ropa - Tranca", productCost: 200 },
+    { productId: "2", productName: "Casaca", productType: "Ropa - Tranca", productCost: 100 },
+    { productId: "3", productName: "Pantalón", productType: "Ropa - Tranca", productCost: 150 },
+    { productId: "4", productName: "Short", productType: "Ropa - Tranca", productCost: 250 },
+    { productId: "5", productName: "Gorro", productType: "Accesorios - Act4u", productCost: 45 },
+    { productId: "6", productName: "Lentes de sol", productType: "Accesorios - Act4u", productCost: 90 },
+    { productId: "7", productName: "Mochila", productType: "Accesorios - Act4u", productCost: 120 },
+    { productId: "8", productName: "Audífonos", productType: "Tecnología - Now", productCost: 50 },
+    { productId: "9", productName: "Parlante", productType: "Tecnología - Now", productCost: 100 },
 ];
 
-//* Método que recorrerá la data y llama al método ".addGame()" para agregarlos a "list" */
+// Método que recorrerá la data y llama al método ".addProduct()" para agregarlos a "productList"
 data.forEach((ele) => {
-    const register = new Juegos(ele.gameId, ele.name, ele.type, ele.cost);
-    register.addGame();
+    const register = new Products(ele.productId, ele.productName, ele.productType, ele.productCost);
+    register.addProduct();
 
 })
 
-//* Declaración de una constante que almacerá los resultados de la búsqueda para luego sumar los costos (cost) */
+// Declaración de una constante que almacerá los resultados de la búsqueda para luego sumar los costos (productCost)
 const cart = [];
 
-//* Inicio de interacción entre el usuario y la web */
-let cliente = prompt("Ingrese su usuario");
+// Inicio de interacción entre el usuario y la web
+let username = prompt("Ingrese su usuario");
 
-//*  Validación de ingreso de usuario */
-while (cliente == "") {
-    cliente = prompt("Verifique su usuario");
+//  Validación de ingreso de usuario
+while (username == "") {
+    username = prompt("Verifique su usuario");
 }
 
-//* Solicitud de código de producto */
-let id = prompt(`Ingrese el código de juego a consultar`);
+// Solicitud de código de producto
+let id = prompt(`
+Ingrese el código del producto (1-9) que agregará al carrito
 
-//* Validación de código de producto */
-while (id != "X") {
-    //* Declarar la variable que filtrará la data para encontrar coincidencias con el id del producto */
-    let filterGame = list.filter(ele => ele.gameId === id);
+Para terminar, escriba 0
+`
+);
 
-    //* Validación de ingreso de id diferente de vacío */
-    if (filterGame.length > 0) {
-        //* Método para agregar los producto filtrados a la constante "cart" */
-        cart.push(filterGame[0]);
+// Validación de código de producto
+while (id != 0) {
+    // Declarar la variable que filtrará la data para encontrar coincidencias con el id del producto
+    let filterProduct = productList.filter(ele => ele.productId === id);
 
-        //* Información a mostrar */
+    // Validación de ingreso de id diferente de vacío
+    if (filterProduct.length > 0) {
+        // Método para agregar los producto filtrados a la constante "cart"
+        cart.push(filterProduct[0]);
+
+        // Información a mostrar
         let info =
             `
-    Información del juego consultado:
+    Información del producto consultado:
 
-    GameID: ${filterGame[0].gameId}
-    Name: ${filterGame[0].name}
-    Category: ${filterGame[0].type}
-    Cost: ${filterGame[0].cost}
+    Código: ${filterProduct[0].productId}
+    Producto: ${filterProduct[0].productName}
+    Categoría: ${filterProduct[0].productType}
+    Costo: $${filterProduct[0].productCost}
     
     ¡Suerte!
     `;
         alert(info);
     } else {
-        alert(`El juego en consulta no se encuentra en la lista`);
+        alert(`El producto en consulta no se encuentra en la lista de productos`);
     }
 
-    id = prompt(`Ingrese el código de juego a consultar`);
+    id = prompt(`
+Ingrese el código del producto (1-9) que agregará al carrito
+
+Para terminar, escriba 0
+`
+    );
 }
 
-//* Declaración de la variable que almacenará el costo de los productos */
+// Declaración de la variable que almacenará el costo de los productos
 let total = 0;
 
-//* Método que recorrerá los productos agregados a "cart" y acumulará el "total" */
+// Método que recorrerá los productos agregados a "cart" y acumulará el "total"
 cart.forEach((ele) => {
-    total = total + Number(ele.cost);
+    total = total + Number(ele.productCost);
 })
 
-//* Condición para aplicar un descuento a través de una operación aritmética */
-if (total > 200) {
+// Condición para aplicar un descuento a través de una operación aritmética
+if (total >= 500) {
     alert(`¡Felicitaciones! Ahora solo paga $${total * 0.75} con el descuento de 25%`);
 
 } else {
     alert(`¡Gracias! El costo total es de $${total}`);
 }
 
-//* Mensaje de cierre de interacción */ 
-alert(`Gracias, ${cliente} ¡Vuelve pronto (=*.*=) !`);
+// Mensaje de cierre de interacción
+alert(`Gracias, ${username} ¡Vuelve pronto (=*.*=) !`);
 
