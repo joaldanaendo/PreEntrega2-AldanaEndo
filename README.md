f2401c72fde6a6eafbd617164af819513e29a1f2
joaldanaendo <johanny.aldana.endo@gmail.com>
2023-09-14 01:25:43 -0500

    Update - Implemetación de fetch (async/await) y carga por archivo local .json

--- README.md
+++ README.md
@@ -38,12 +38,19 @@ Repositorio para la entrega de trabajos del curso de JavaScript
 
 # Desarrollo
 
-- En la web, el usuario puede iniciar sesión para identificarse.
+- En la web, el usuario puede iniciar sesión para identificarse, esto es opcional, puesto que también se presenta la opción para continuar sin sesión.
 - Esta sesión se guardará en el localStorage, cerrando la sesión solo al finalizar la compra.
-- Se muestra la página de la tienda con 9 productos que se ingresan a través de la creación de un arreglo (cart []), que se llenará con la información de una constante llamada productos con el método forEach.
+- Se muestra la página de la tienda con 12 productos que se ingresan a través de la creación de un archivo .json, cuya información se cargará de manera asíncrona con uso de async/await como sustitución de .then en más de una oportunidad.
 - Se agrega el elemto botón por medio de un elemento identificado con la clase .btnAddProduct y se le asigna la posibilidad de poder filtrar, con un clic llamado con un addEventListener y de identificar los productos, así sean la misma prenda, con un identicador agregado con la librería "uuid", de este modo, si se elimina un producto X y existiese más productos iguales, el total siga considerando los productos que quedan realmente en el carrito.
 - Con appendCart se agrega internamente al contenedor del carrito y se le asigna una tarjeta agregada con bootstrap.
 - Se crea un elemento button por cada producto agregado al carrito, de este modo, se podrán eliminar los productos. Estos elementos mantienen la identificación con uuid.
 - Se calcula el total, considerando las posibles eliminaciones de los productos.
 - Muestra el total.
 - Se agrega un botón para finalizar la compra y mostrar un mensaje de compra exitosa.
+
+# Indicaciones finales
+
+- No usar prompt, alert ni console.log
+- Uso de operadores ternarios
+- Manejo de promesas con fetch (asincronía)
+- Carga de datos desde API o archivo local .json
--- js/main.js
+++ js/main.js
@@ -1,8 +1,3 @@
-// Información referente a los productos
-
-// Contenedor de productos
-let contenedor = document.getElementById("contenedor-productos");
-
 // Declaración de arreglo para recibir productos en el carrito
 let carritoReceptor = [];
 
@@ -12,56 +7,63 @@ if (carritoAlmacenado) {
   carritoReceptor = JSON.parse(carritoAlmacenado);
 }
 
-// Arreglo de objetos con las características de los productos
-const productos = [
-    { productoId: "1", productoNombre: "Remera", productoImagen:"./assets/img/products/id1.jpg", productoColor: "negro", productoCosto: 13500},
-    { productoId: "2", productoNombre: "Campera", productoImagen:"./assets/img/products/id2.jpg" ,productoColor: "negro", productoCosto: 65000},
-    { productoId: "3", productoNombre: "Short", productoImagen:"./assets/img/products/id3.jpg" ,productoColor: "negro", productoCosto: 20000},
-    { productoId: "4", productoNombre: "Gorro", productoImagen:"./assets/img/products/id4.jpg" ,productoColor: "negro", productoCosto: 7500},
-    { productoId: "5", productoNombre: "Pantalón", productoImagen:"./assets/img/products/id5.jpg" ,productoColor: "blanco", productoCosto: 45000},
-    { productoId: "6", productoNombre: "Lentes ", productoImagen:"./assets/img/products/id6.jpg" ,productoColor: "negro", productoCosto: 10000},
-    { productoId: "7", productoNombre: "Buzo", productoImagen:"./assets/img/products/id7.jpg" ,productoColor: "perla", productoCosto: 33000},
-    { productoId: "8", productoNombre: "Jean", productoImagen:"./assets/img/products/id8.jpg" ,productoColor: "azul", productoCosto: 85000},
-    { productoId: "9", productoNombre: "Campera", productoImagen:"./assets/img/products/id9.jpg" ,productoColor: "beige", productoCosto: 65000},
-    { productoId: "10", productoNombre: "Pantuflas", productoImagen:"./assets/img/products/id10.jpg" ,productoColor: "celeste", productoCosto: 25000},
-    { productoId: "11", productoNombre: "Gorro", productoImagen:"./assets/img/products/id11.jpg" ,productoColor: "beige", productoCosto: 7500},
-    { productoId: "12", productoNombre: "Bolsa", productoImagen:"./assets/img/products/id12.jpg" ,productoColor: "beige", productoCosto: 2500},
-];
+// Inserción de productos desde .json
+let contenedor = document.getElementById("contenedor-productos");
 
-// Método para agregar la información a un div del HTML
-productos.forEach((item) => {
-  let productoDeCarrito = document.createElement("div");
-  productoDeCarrito.className = "productoDeCarrito";
-  productoDeCarrito.innerHTML = `
-    <h2>${item.productoNombre}</h2>
-    <img src= ${item.productoImagen} class= imgProduct alt= ${item.productoNombre} style="width: auto; height:15rem">
-    <h5>Color: ${item.productoColor}</h5>
-    <b>$${item.productoCosto}</b>
-    <button type="button" class = "btnAddProduct" value = ${item.productoId} >Agregar al carrito</button>
-  `;
-  contenedor.append(productoDeCarrito);
-});
+const traerProductos = async () => {
+  try {
+    const response = await fetch("../products.json");
+    const catalogo = await response.json();
 
-// Función que se le asigna al botón "addProduct"
-let botonAgregarAlCarrito = document.getElementsByClassName("btnAddProduct");
-for (let i = 0; i < botonAgregarAlCarrito.length; i++) {
-  botonAgregarAlCarrito[i].addEventListener("click", (e) => {
-let productoIdEnCarrito = productos.filter(ele => ele.productoId == e.target.value);
-let identificadorId = uuid.v1();
-carritoReceptor.push({...productoIdEnCarrito[0], identifier:identificadorId});
-appendCart({...productoIdEnCarrito[0], identifier:identificadorId});
-pagoTotal(productoIdEnCarrito[0].productoCosto);
+    catalogo.forEach((item) => {
+      const div = document.createElement("div");
+      div.className = "productoDeCatalogo";
+      div.innerHTML = `
+      <h2>${item.productoNombre}</h2>
+      <img src= ${item.productoImagen} class= imgProduct alt= ${item.productoNombre} style="width: auto; height:15rem">
+      <h5>Color: ${item.productoColor}</h5>
+      <b>$${item.productoCosto}</b>
+      <button type="button" class = "btnAddProduct" value = ${item.productoId} >Agregar al carrito</button>
+    `;
+      contenedor.append(div);
+    });
 
-// Actualización localStorage
-localStorage.setItem("carrito", JSON.stringify(carritoReceptor));
-})}
+    // Función que se le asigna al botón "addProduct"
+    let botonAgregarAlCarrito =
+      document.getElementsByClassName("btnAddProduct");
+    for (let i = 0; i < botonAgregarAlCarrito.length; i++) {
+      botonAgregarAlCarrito[i].addEventListener("click", (e) => {
+        let productoIdEnCarrito = catalogo.filter(
+          (ele) => ele.productoId == e.target.value
+        );
+        let identificadorId = uuid.v1();
+        carritoReceptor.push({
+          ...productoIdEnCarrito[0],
+          identifier: identificadorId,
+        });
+        appendCart({ ...productoIdEnCarrito[0], identifier: identificadorId });
+        pagoTotal(productoIdEnCarrito[0].productoCosto);
+
+        // Actualización localStorage
+        localStorage.setItem("carrito", JSON.stringify(carritoReceptor));
+      });
+    }
+  } catch (error) {
+    const div = document.createElement("div");
+    div.innerHTML = `Error ${error}`;
+    document.body.append(div);
+  }
+};
+
+// Se invoca a la función que llame a los productos
+traerProductos();
 
 // Declaración e inicialización de variable que brindará el total
-let total=0;
+let total = 0;
 
 // Función para agregar items al carrito
 const appendCart = (item) => {
-// Contenedor de carrito
+  // Contenedor de carrito
   let contenedorCarrito = document.getElementById("contenedor-carrito");
   let productoIdEnCarritoMini = document.createElement("div");
   productoIdEnCarritoMini.className = "productoIdEnCarritoMini";
@@ -81,77 +83,81 @@ const appendCart = (item) => {
     </div>
   </div>
 </div>
-`
-// Contenedor visual
+`;
+  // Contenedor visual
   contenedorCarrito.append(productoIdEnCarritoMini);
   let botonBorrar = document.createElement("button");
   botonBorrar.id = "myButtonDelete";
   botonBorrar.className = "myButtonDeleteClass";
   botonBorrar.textContent = "Borrar";
   botonBorrar.addEventListener("click", () => {
-    let borrarItem = carritoReceptor.filter(ele => ele.identifier !== item.identifier);
+    let borrarItem = carritoReceptor.filter(
+      (ele) => ele.identifier !== item.identifier
+    );
     carritoReceptor = borrarItem;
 
-// Actualiza localStorage
-localStorage.setItem("carrito", JSON.stringify(carritoReceptor));
+    // Actualiza localStorage
+    localStorage.setItem("carrito", JSON.stringify(carritoReceptor));
 
-  let getProduct = document.getElementById(item.identifier);
-  getProduct.remove();
-  pagoTotal(item.productoCosto*-1);
-  })
+    let getProduct = document.getElementById(item.identifier);
+    getProduct.remove();
+    pagoTotal(item.productoCosto * -1);
+  });
   let contenedorBotonBorrar = document.getElementById(`${item.identifier}`);
   contenedorBotonBorrar.appendChild(botonBorrar);
-}
+};
 
 window.addEventListener("load", () => {
-  if(carritoReceptor.length > 0){
+  if (carritoReceptor.length > 0) {
     carritoReceptor.forEach((item) => {
-    appendCart(item);
-    pagoTotal(item.productoCosto);
-    })}
-})
+      appendCart(item);
+      pagoTotal(item.productoCosto);
+    });
+  }
+});
 
 // Llamado a función que oculatará el botón hasta que se agreguen productos
 ocultarBtnFinalizar();
 
 // Función para cálculo total
-function pagoTotal(precio){
-   
-    total = total + precio;
-    let totalAgregado = document.getElementById("total");
-    totalAgregado.textContent = `Cantidad: ${carritoReceptor.length} - Total a pagar: $${total}`;
-    // Condición para mostrar el botón finalizar solo si hay productos en el carrito
-    if(carritoReceptor.length > 0){
-      mostrarBtnFinalizar();
-    } else {
-      ocultarBtnFinalizar();
-    }
+function pagoTotal(precio) {
+  total = total + precio;
+  let totalAgregado = document.getElementById("total");
+  totalAgregado.textContent = `Cantidad: ${carritoReceptor.length} - Total a pagar: $${total}`;
+  // Condición para mostrar el botón finalizar solo si hay productos en el carrito
+  if (carritoReceptor.length > 0) {
+    mostrarBtnFinalizar();
+  } else {
+    ocultarBtnFinalizar();
   }
-  
+}
+
 // Método para indicar qué mostrar con el botón "Finalizar compra"
-  let btnFinalizar = document.getElementById("btnFinalizar");
-  btnFinalizar.addEventListener("click", () => {
-    Swal.fire({
-      title: 'Gracias por comprar Tranca',
-      text: `N° orden: ${Math.floor(Math.random() * 5000)} - Tu cuenta es de $${total}`,
-      imageUrl: './assets/img/tranca/tranca_final.jpg',
-      imageWidth: 300,
-      imageHeight: 300,
-      imageAlt: 'Tranca perrita',
-      showDenyButton: true,
-      showCancelButton: false,
-      confirmButtonText: 'Finalizar',
-      denyButtonText: `Regresar`,
-    }).then((result) => {
-      result.isConfirmed ? window.location.reload() : result.isDenied;
-    })
-// Eliminación de localStorage al finalizar la compra
-    localStorage.clear();
+let btnFinalizar = document.getElementById("btnFinalizar");
+btnFinalizar.addEventListener("click", () => {
+  Swal.fire({
+    title: "Gracias por comprar Tranca",
+    text: `N° orden: ${Math.floor(
+      Math.random() * 5000
+    )} - Tu cuenta es de $${total}`,
+    imageUrl: "./assets/img/tranca/tranca_final.jpg",
+    imageWidth: 300,
+    imageHeight: 300,
+    imageAlt: "Tranca perrita",
+    showDenyButton: true,
+    showCancelButton: false,
+    confirmButtonText: "Finalizar",
+    denyButtonText: `Regresar`,
+  }).then((result) => {
+    result.isConfirmed ? window.location.reload() : result.isDenied;
   });
+  // Eliminación de localStorage al finalizar la compra
+  localStorage.clear();
+});
 
 // Validación para carga de localStorage de usuario o solicitar el ingreso
 let usuario = localStorage.getItem("usuario");
-usuario && mostrarUsuario(usuario)
+usuario && mostrarUsuario(usuario);
 
 // Función para solicitar el inicio de la sesión
 function mostrarUsuario(usuario) {
@@ -159,50 +165,51 @@ function mostrarUsuario(usuario) {
   infoUsuario.textContent = `¡Qué gusto verte nuevamente, ${usuario}!`;
 
   // Condición para mostrar el botón login o no en función a la existencia de usuario en el localStorage
-  (localStorage.getItem("usuario") !== null) ? ocultarbtnInicioSesion() : mostrarbtnInicioSesion();
-  }
-
+  localStorage.getItem("usuario") !== null
+    ? ocultarbtnInicioSesion()
+    : mostrarbtnInicioSesion();
+}
 
 // Botón de inicio de sesión
 let btnInicioSesion = document.getElementById("btnInicioSesion");
 btnInicioSesion.addEventListener("click", () => {
   Swal.fire({
-    title: 'Inicie sesión',
-    input: 'text',
-    inputPlaceholder: 'Ingrese su usuario',
+    title: "Inicie sesión",
+    input: "text",
+    inputPlaceholder: "Ingrese su usuario",
     inputAttributes: {
-      autocapitalize: 'off'
+      autocapitalize: "off",
     },
     showCancelButton: true,
-    confirmButtonText: 'Iniciar Sesión',
-    cancelButtonText: 'Continuar sin sesión',
+    confirmButtonText: "Iniciar Sesión",
+    cancelButtonText: "Continuar sin sesión",
     preConfirm: (usuario) => {
       if (usuario) {
         // Guarda el nombre de usuario en el localStorage del usuario
         localStorage.setItem("usuario", usuario);
         mostrarUsuario(usuario);
 
-// Valida la aparación del botón en función al localStorage
-        btnInicioSesion.style.display = 'none';
+        // Valida la aparación del botón en función al localStorage
+        btnInicioSesion.style.display = "none";
       }
-    }
+    },
   });
 });
 
 // Funciones para mostrar u ocultar el botón "Finalizar compra"
-function ocultarBtnFinalizar(){
+function ocultarBtnFinalizar() {
   document.getElementById("btnFinalizar").style.display = `none`;
 }
 
-function mostrarBtnFinalizar(){
+function mostrarBtnFinalizar() {
   document.getElementById("btnFinalizar").style.display = "block";
 }
 
 // Funciones para mostrar u ocultar el botón "Login/Iniciar Sesión"
-function ocultarbtnInicioSesion(){
+function ocultarbtnInicioSesion() {
   document.getElementById("btnInicioSesion").style.display = `none`;
 }
 
-function mostrarbtnInicioSesion(){
+function mostrarbtnInicioSesion() {
   document.getElementById("btnInicioSesion").style.display = "block";
 }
